e902b2afe9e297d3dde1752d1cf34807622822d6
benlongcroft <blongcroft@gmail.com>
2022-02-28 09:41:52 +0000

    added metadata to gallery

--- Gallery/views.py
+++ Gallery/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 import os
 import random
+from exif import Image
 
 gallery_blueprint = Blueprint('gallery', __name__, template_folder='templates')
 
@@ -8,6 +9,13 @@ gallery_blueprint = Blueprint('gallery', __name__, template_folder='templates')
 @gallery_blueprint.route('/gallery')
 def gallery():
     return render_template("gallery.html", get_image_set=get_image_set)
+
+
+def get_meta(path):
+    with open(path, 'rb') as image_file:
+        my_image = Image(image_file)
+        if my_image.has_exif:
+            return {"make":my_image.make, "model":my_image.model, "datetime": my_image.datetime_original}
 
 
 def get_image_set():
@@ -20,7 +28,9 @@ def get_image_set():
         if img_path == ".DS_Store":
             continue
         p = os.path.join(assets_path, img_path)
-        image_set.append({'path': p, 'name': get_image_name(p), 'description': None})
+        print(p)
+        meta = get_meta("static/"+p)
+        image_set.append({'path': p, 'name': get_image_name(p), 'description': None, "meta" : meta})
         # TODO: implement this so that description gets shop metadata
     return image_set
 
